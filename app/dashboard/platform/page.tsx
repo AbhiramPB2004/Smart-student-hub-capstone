174fb0852a4e4606c7d842ccb46b3db822528b47
abhiram PB <109417244+AbhiramPB2004@users.noreply.github.com>
2026-02-04 15:02:53 +0530

    Update page.tsx

--- app/dashboard/platform/page.tsx
+++ app/dashboard/platform/page.tsx
@@ -28,11 +28,7 @@ export default function PlatformDashboard() {
                   View
                 </button>
               </Link>
-              <Link href="/dashboard/platform/super-admins/create">
-                <button className="bg-green-600 text-white px-4 py-2 rounded">
-                  Create
-                </button>
-              </Link>
+
             </div>
           </div>
 
